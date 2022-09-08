c0a3f28e0355239e3db78a83c221a2b951cae6b8
toni <easyrobtics2@gmail.com>
2022-09-08 13:34:45 +0200

    Add gitignore

--- holamundo.c
+++ holamundo.c
@@ -1,13 +1,26 @@
+
 #include <stdio.h>
 
 int main () {
 
-    char nombre[100];
-    printf("Escribe tu nombre:");
-    /* leemos el teclado */
+    char nombre[10];
+    int edad;
+    printf("Escribe tu nombre: ");
+    /* leemos el nombre de teclado */
     scanf("%s", nombre);
-    /* imprimimos en pantalla */
-    printf("Bienvenido %s!\n",nombre);
+    /*leemos la edad */
+    printf("Escribe tu edad: ");
+    scanf("%d", &edad);
+
+    if(edad > 17){
+        /* imprimimos en pantalla */
+        printf("Bienvenido %s, tienes %d años.\n", nombre, edad);
+    }else if(edad == 17){
+        printf("Te esperamos, casi tienes 18!!");
+    }else{
+        printf("Eres menor de edad, no tienes acceso!\n");
+    }
+    
 
     return 0;
 }
